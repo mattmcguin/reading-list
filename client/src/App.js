22481431b1d1f612827bf054ae38ee018bd92645
Matt McGuiness <mattjmcguiness@gmail.com>
2018-05-31 18:20:58 -0400

    🎉Finish Front-End

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+import BookList from "./components/BookList";
+
+const client = new ApolloClient({
+    uri: "http://localhost:4000/graphql"
+});
+
+const Main = styled.div`
+    box-sizing: border-box;
+    padding: 0;
+    width: 60%;
+    height: 100%;
+
+    @media (max-width: 700px) {
+        width: 100%;
+    }
+`;
+
+const Title = styled.h1`
+    color: #222022;
+    text-align: center;
+`;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <ApolloProvider client={client}>
+                <Main>
+                    <Title>Reading List</Title>
+                    <BookList />
+                </Main>
+            </ApolloProvider>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/components/BookDetails.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import { getBookQuery } from "../queries/queries";
+import styled from "styled-components";
+import AddBook from "./AddBook";
+
+const Container = styled.div`
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 40%;
+    height: 100%;
+    background: #6ead3a;
+    padding: 30px;
+    overflow: auto;
+    box-shadow: -2px -3px 5px rgba(0, 0, 0, 0.3);
+    box-sizing: border-box;
+    color: #ffffff;
+
+    @media (max-width: 700px) {
+        position: relative;
+        width: 100%;
+        padding: 0;
+    }
+`;
+
+const Book = styled.div`
+    @media (max-width: 700px) {
+        padding: 30px;
+    }
+`;
+
+class BookDetails extends Component {
+    renderBookDetails = () => {
+        const { book } = this.props.data;
+
+        if (book) {
+            return (
+                <Book>
+                    <h2>{book.name}</h2>
+                    <p>Genre: {book.genre}</p>
+                    <p>Author: {book.author.name}</p>
+                    <p>All books by this author:</p>
+                    <ul>
+                        {book.author.books.map(item => {
+                            return <li key={item.id}>{item.name}</li>;
+                        })}
+                    </ul>
+                </Book>
+            );
+        } else {
+            return <Book>No Book Selected</Book>;
+        }
+    };
+
+    render() {
+        return (
+            <Container>
+                {this.renderBookDetails()}
+                <AddBook />
+            </Container>
+        );
+    }
+}
+
+export default graphql(getBookQuery, {
+    options: props => {
+        return {
+            variables: {
+                id: props.bookId
+            }
+        };
+    }
+})(BookDetails);
